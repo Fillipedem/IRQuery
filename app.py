c94367304eb917d24b37a0a402fbcc13475c7402
fillipedem <fillipedem@gmail.com>
2019-11-20 20:53:39 -0300

    Mudanças na requisições

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, request
 from query import Query, ZoneQuery
 from score import CosineScore
 from index import ReverseIndex
+from flask import json
 
 #index
 print("Lendo Indices")
@@ -20,6 +21,31 @@ zone_index = ReverseIndex(zone_index_path)
 index_score = CosineScore(index)
 advanced_score = CosineScore(zone_index)
 
+#
+def load_json(path):
+    data = {}
+    with open(path, 'r') as json_file:
+        data = json.load(json_file)
+
+    return data
+
+filtered_data = load_json('./filtered_datastore.json')
+
+# docs
+def get_docs(docIDs):
+    ans = []
+
+    for docID in docIDs:
+        data = load_json('./extract/' + str(docID))
+        for fd in filtered_data:
+            if fd['id'] == docID:
+                data['url'] = fd['url']
+                ans.append(data)
+                break
+
+    return ans
+
+
 # Flask interface
 app = Flask(__name__)
 
@@ -30,7 +56,15 @@ def search():
     query = Query(free_text)
     scores = index_score.score(query)
 
-    return str(scores)
+    docs = get_docs(scores)
+
+    response = app.response_class(
+        response=json.dumps(docs),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
 
 @app.route('/advanced_search')
 def advanced_search():
@@ -42,7 +76,15 @@ def advanced_search():
     query = ZoneQuery(parameters)
     scores = advanced_score.score(query)
 
-    return str(scores)
+    docs = get_docs(scores)
+
+    response = app.response_class(
+        response=json.dumps(docs),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
 
 
 def valid_params(params):
